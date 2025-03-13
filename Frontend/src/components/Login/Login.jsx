b39c1b57f2526ec3a40c2eadc899b79f0da6ec03
Vansh <vanshgarg05@gmail.com>
2025-03-13 22:14:04 +0530

    Login and Signup page

--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import "./Login.css";
 
 const Login = () => {
   return (
-    <div>
-      login
-    </div>
-  )
-}
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Welcome Back</h2>
+        <p>Login to continue</p>
+        <form>
+          <div className="input-group">
+          <label>Email</label>
+            <input type="email" required />
+            
+          </div>
+          <div className="input-group">
+            <label>Password</label>
+            <input type="password" required />
+           
+          </div>
+          <button type="submit" className="login-btn">Login</button>
+        </form>
+        
+        <p className="signup-link">
+          Don't have an account? <a href="#">Sign Up</a>
+        </p>
 
-export default Login
+        <div className="social-icons">
+          <img src="/icons/google-symbol.png" alt="Google" className="social-icon" />
+          <img src="/icons/github.png" alt="GitHub" className="social-icon" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
